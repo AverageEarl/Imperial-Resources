8ca1c1960dde514098f9913656bcb381bb0a4e9e
ec <Eugen.Creutz@gmail.com>
2024-06-10 15:13:34 +0200

    first fuctional version

---
+++ Zip.bat
@@ -0,0 +1,1 @@
+tar -C %~dp0imperialResources -a -c -f ImperialResources.zip assets pack.png pack.mcmeta
--- test.bat
+++
@@ -1,4 +0,0 @@
-tar -C %~dp0imperialResources -a -c -f ImperialResources.zip assets pack.png pack.mcmeta
-
-FOR /F "tokens=*" %%i IN (path.txt) DO @ECHO %%i
-pause
